410f2c3ab2d08c1837c45dd4b20de0600750469c
SantanaGG <52512306+SantanaGG@users.noreply.github.com>
2019-08-01 13:57:33 -0300

    Update Diagrama de Venn.R

--- Diagrama de Venn.R
+++ Diagrama de Venn.R
@@ -1,15 +1,15 @@
 1º Metodo
 venn.diagram(x, #vetor que será usado no diagrama
              filename = "nome do arquivo que será exportado.PNG ou .tiff",
-             scaled = FALSE,
-             euler.d = FALSE, 
+             scaled = FALSE, #escalonamento 
+             euler.d = FALSE, #escalonamento de euler para diagrama
               resolution = 600,
              height = 2500, #altura
              widht = 2500, #largura
-             cat.cex = .75,
-             cex = .75, 
-             lty = 2, 
-             lwd = 0.5,
+             cat.cex = .75, #tamanho dado para cada nome de categoria
+             cex = .75, #tamanho para cada rótulo de área
+             lty = 2, #padrão de traço de cada circulo
+             lwd = 0.5, #largura da circunferencia de cada circulo
              fontfamily = "serif",
              imagetype = "tiff ou png",
              fill = c("cor primeiro circulo","cor segundo circulo"),
@@ -21,12 +21,12 @@ venn.diagram(x, #vetor que será usado no diagrama
 2º Metodo
 grid.newpage()
 png(filename = "nome do arquivo que será exportado.PNG ou .tiff")
-venn.plot <- draw.pairwise.venn(area1 = 680,
-                                area2 = 540,
-                                cross.area = 509,
-                                category = c("DAY 14","DAY 7"),
-                                cat.pos = c(0,0),
-                                cat.cex = 2,
+venn.plot <- draw.pairwise.venn(area1 = 680, #numero do primero circulo
+                                area2 = 540, #numero do segund circulo
+                                cross.area = 509, #numero que contem em ambos
+                                category = c("DAY 14","DAY 7"), #titulo dos circulos
+                                cat.pos = c(0,0), #posição do titulo
+                                cat.cex = 2, #tamanho dado para cada nome de categoria
                                 lty = ("blank"), 
                                 fill = c("cor primeiro circulo","cor segundo circulo"),
                                 cat.dist = rep(0.025, 2),
